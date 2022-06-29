266bff4a454410b046eac0f675498e5c5a669b4f
Heo Eun <64454313+DAASHeo@users.noreply.github.com>
2022-06-30 00:40:37 +0900

    4일차

---
+++ 4일차/3.py
@@ -0,0 +1,16 @@
+class Car:
+    carname = ""
+    speed = 0
+
+Car_1 = Car()
+Car_1.carname = "아우디"
+Car_1.speed = 0
+Car_2 = Car()
+Car_2.carname = "벤츠"
+Car_2.speed = 30
+
+def present(Carvalue1, Carvalue2):
+    print(Carvalue1.carname,"의 현재 속도는",Carvalue1.speed,"입니다.")
+    print(Carvalue2.carname,"의 현재 속도는",Carvalue2.speed,"입니다.")
+
+present(Car_1,Car_2)
